9b0242ec2218411fd25953ad1573e8532921523c
David Kitchen <david@committeehq.com>
2020-05-27 13:44:33 +0100

    Add and use ChildEntityRouteContextTrait.php

---
+++ src/Context/ChildEntityRouteContextTrait.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\child_entity\Context;
+
+use Drupal\child_entity\Entity\ChildEntityInterface;
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * Trait to get the parent entity from the current route.
+ *
+ * Using this trait will add the getParentEntityFromRoute() method to the class.
+ *
+ * If the class is capable of injecting services from the container, it should
+ * inject the 'current_route_match' service and
+ * assign them to the currentRouteMatch and entityTypeManager properties.
+ */
+trait ChildEntityRouteContextTrait {
+
+  /**
+   * The current route match object.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $currentRouteMatch;
+
+  /**
+   * Gets the current route match object.
+   *
+   * @return \Drupal\Core\Routing\RouteMatchInterface
+   *   The current route match object.
+   */
+  protected function getCurrentRouteMatch() {
+    if (!$this->currentRouteMatch) {
+      $this->currentRouteMatch = \Drupal::service('current_route_match');
+    }
+    return $this->currentRouteMatch;
+  }
+
+  /**
+   * Retrieves the parent entity from the current route.
+   *
+   * This will try to load the parent entity from the route if present. If we are
+   * on the group add form, it will return a new group entity with the group
+   * type set.
+   *
+   * @param ChildEntityInterface $child_entity
+   * @return EntityInterface|null
+   *   The parent entity if one could be found, NULL otherwise.
+   */
+  public function getParentEntityFromRoute(ChildEntityInterface $child_entity) {
+    $route_match = $this->getCurrentRouteMatch();
+
+    // See if the route has a group parameter and try to retrieve it.
+    if ($parent_entity = $route_match->getParameter($child_entity->getParentEntityTypeId())) {
+      return $parent_entity;
+    }
+
+    return NULL;
+  }
+
+}
--- src/Form/ChildContentEntityForm.php
+++ src/Form/ChildContentEntityForm.php
@@ -4,43 +4,40 @@ namespace Drupal\child_entity\Form;
 
 use Drupal\child_entity\Entity\ChildEntityInterface;
 use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\child_entity\Context\ChildEntityRouteContextTrait;
 
 abstract class ChildContentEntityForm extends ContentEntityForm {
+
+  use ChildEntityRouteContextTrait;
+
+  /**
+   * The entity being used by this form.
+   *
+   * @var ChildEntityInterface
+   */
+  protected $entity;
 
   /**
    * @inheritDoc
    */
   public function save(array $form, FormStateInterface $form_state) {
-    /** @var ChildEntityInterface $entity */
-    $entity = $this->entity;
 
-    $entity->setParentEntity($this->getParentEntity());
+    $this->entity->setParentEntity($this->getParentEntity());
     return parent::save($form, $form_state);
   }
 
   /**
-   * @return \Drupal\Core\Entity\EntityInterface
+   * @return EntityInterface
    */
   protected function getParentEntity() {
     if ($this->entity->isNew()) {
-      return $this->getParentEntityFromRoute();
+      return $this->getParentEntityFromRoute($this->entity);
     }
     else {
       return $this->entity->getParentEntity();
     }
   }
 
-  /**
-   * @return \Drupal\Core\Entity\EntityInterface
-   */
-  private function getParentEntityFromRoute() {
-    /** @var ChildEntityInterface $entity */
-    $entity = $this->entity;
-
-    return \Drupal::getContainer()
-      ->get('request_stack')
-      ->getMasterRequest()
-      ->get($entity->getParentEntityTypeId());
-  }
 }
